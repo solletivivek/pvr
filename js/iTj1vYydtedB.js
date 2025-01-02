9fa777a1cc592f7eb253b74ca92487bdcbc8170a
solletivivek <2100031220@kluniversity.in>
2025-01-02 17:30:07 +0530

    Refactor index.html and clean up unused comments in iTj1vYydtedB.js

--- js/iTj1vYydtedB.js
+++ js/iTj1vYydtedB.js
@@ -1,12 +1,4 @@
 
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-
 (() => {
   var __getOwnPropNames = Object.getOwnPropertyNames;
   var __commonJS = (cb, mod) => function __require() {
